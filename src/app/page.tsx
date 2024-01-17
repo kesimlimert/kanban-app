93001fd3bfc0653b1e82657391068c587947966a
kesimlimert <mert_cod@hotmail.com>
2024-01-17 23:08:14 +0300

    env updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 import Board from "@/components/board/board";
 
 export default async function Home() {
-  const data = await fetch(`${process.env.BASE_URL}/api/content`).then((res) => res.json());
+  const data = await fetch(process.env.URL + '/api/content').then((res) => res.json());
   return (
     <main className={styles.main}>
       <Board data={data} />
