3346c759d30d810b319ab8e05043a939a9857423
kesimlimert <mert_cod@hotmail.com>
2024-01-17 23:00:41 +0300

    msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Board from "@/components/board/board";
 
-export default async function Home({}) {
+export default async function Home() {
   const data = await fetch(`${process.env.BASE_URL}/api/content`).then((res) => res.json());
-
   return (
     <main className={styles.main}>
       <Board data={data} />
